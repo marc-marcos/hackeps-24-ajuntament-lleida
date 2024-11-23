cea37affa2c21347e315859129e946fd3920be78
paula <papechia@gmail.com>
2024-11-23 22:11:32 +0100

    map changed to folium

--- frontend-web/pages/map.py
+++ frontend-web/pages/map.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests as req
+import folium
+from streamlit_folium import folium_static
 
 url = "http://127.0.0.1:8000/api/parking"
 
@@ -30,29 +32,30 @@ if 'id_parking' not in st.session_state:
 
 if parquings.status_code == 200:
     parquings = parquings.json()
+    
+    folium_map = folium.Map(location=[latitud_catalunya, longitud_catalunya], zoom_start=10)
+
     for parquing in parquings:
         id = st.session_state.id_parking
         url = f'http://127.0.0.1:8000/api/parking/{id}/plazas'
         response = req.get(url)
-        data2 = response.json()
-        ocuppied = [item["ocupada"] for item in data2]
-        plazas = 0
-        for ocup in ocuppied:
-            if not ocup:
-                plazas += 1
-        if (plazas > 0): colorPoint = "#008000"
-        data = data._append({
-            'latitude': parquing['latitude'],
-            'longitude': parquing['longitude'],
-            'name': parquing['nom'],
-            'size': 50 # tamaño de los marcadores
 
-        }, ignore_index=True)
-    
-    st.map(data[['latitude', 'longitude']], zoom = 3, use_container_width=True, color = colorPoint)
+        if response.status_code == 200:
+            data2 = response.json()
+            ocuppied = [item["ocupada"] for item in data2]
+            plazas = 0
+            for ocup in ocuppied:
+                if not ocup:
+                    plazas += 1
+            if (plazas > 0): colorPoint = "#008000"
+
+            folium.Marker(
+                location=[parquing['latitude'], parquing['longitude']],
+                popup=f"Parquing: {parquing['nom']}<br>Plazas disponibles: {plazas}",
+                icon=folium.Icon(color=colorPoint)
+            ).add_to(folium_map)
+
+        else: st.error("Error al carregar les places del pàrquing")
+    folium_static(folium_map)
 else: st.error("Error al carregar els pàrquings")
-
-
-
-
 
