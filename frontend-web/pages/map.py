e0ebb0032935a7d8adceda3aeacf1f9d4f998c57
paula <papechia@gmail.com>
2024-11-23 17:39:50 +0100

    first map

--- frontend-web/pages/map.py
+++ frontend-web/pages/map.py
@@ -1,6 +1,21 @@
 import streamlit as st
-import constants 
+import pandas as pd
+import numpy as np
+import requests as req
 
-st.write("Hello world")
+# Coordenadas aproximadas del centro de Cataluña (Barcelona)
+latitud_catalunya = 41.5963
+longitud_catalunya = 1.4744
 
-st.write(constants.variable)
+# Crear un DataFrame con las coordenadas
+data = pd.DataFrame({
+    'latitude': [latitud_catalunya],
+    'longitude': [longitud_catalunya],
+    'zoom': [150],
+    'name': ['Catalunya'],
+    'size': [40]
+})
+
+st.title("Localitza un pàrquing")
+st.map(data)
+
