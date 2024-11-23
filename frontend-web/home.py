9a1793e6aad9b80bca03cf5a5896b76b094d8b69
HannahRober <hannah.rober@estudiantat.upc.edu>
2024-11-23 19:40:42 +0100

    parking functions

--- frontend-web/home.py
+++ frontend-web/home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.tile("Home")
+st.title("Home")
 
 if 'id_parking' not in st.session_state:
         st.session_state.id_parking = 1
--- frontend-web/pages/parking.py
+++ frontend-web/pages/parking.py
@@ -1,4 +1,47 @@
 import streamlit as st
+import requests as rq
 
-st.write("Primera página")
+st.title("Distribució del Pàrquing")
 
+if 'id_parking' not in st.session_state:
+        st.session_state.id_parking = 1
+
+id = st.session_state.id_parking
+
+url1 = f'http://127.0.0.1:8000/api/parking/{id}/plazas'
+url2 = f'http://127.0.0.1:8000/api/parking/{id}/plantas'
+
+response = rq.get(url1)
+
+response2 = rq.get(url2)
+
+data = response.json() #plazas ocupadas
+
+
+data_plantas = response2.json()
+num_plantas = 0
+for item in data_plantas:
+        num_plantas += 1
+
+#st.write(num_plantas)
+
+ocuppied = [item["ocupada"] for item in data]
+
+tab_labels = [item["nom"] for item in data_plantas]
+tabs = st.tabs(tab_labels)
+
+for i, tab in enumerate(tabs):
+        with tab:
+            st.subheader(f"Distribució pàrquing ")
+            for ocup in ocuppied:
+                    if ocup:
+                            st.write("ocupat")
+                    else:
+                            st.write("no ocupat")
+
+
+                        
+
+#st.write(ocuppied)
+
+#st.write(data)
