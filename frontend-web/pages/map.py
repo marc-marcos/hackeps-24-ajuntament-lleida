d01b92116f6f3c3688fa11c6e84dbd10acd1e05e
paula <papechia@gmail.com>
2024-11-24 04:05:59 +0100

    map finished

--- frontend-web/pages/map.py
+++ frontend-web/pages/map.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 import requests as req
 import folium
-from streamlit_folium import folium_static
+from streamlit_folium import st_folium
+#from streamlit_autorefresh import st_autorefresh
 
 url = "http://127.0.0.1:8000/api/parking"
 
@@ -11,20 +12,15 @@ url = "http://127.0.0.1:8000/api/parking"
 latitud_catalunya = 41.5963
 longitud_catalunya = 1.4744
 
-# Crear un DataFrame con las coordenadas
-data = pd.DataFrame({
-    'latitude': [latitud_catalunya],
-    'longitude': [longitud_catalunya],
-    'zoom': [150],
-    'name': ['Catalunya'],
-    'size': [40]
-})
+if "selected_parking" not in st.session_state:
+    st.session_state.selected_parking = None
+
+#st_autorefresh(interval=2000)
 
 parquings = req.get(url)
 
 #valor per default null
 data = pd.DataFrame(columns=['latitude', 'longitude', 'name', 'size'])
-colorPoint = "#ff0000"
 st.title("Localitza un pàrquing")
 
 if 'id_parking' not in st.session_state:
@@ -33,10 +29,11 @@ if 'id_parking' not in st.session_state:
 if parquings.status_code == 200:
     parquings = parquings.json()
     
-    folium_map = folium.Map(location=[latitud_catalunya, longitud_catalunya], zoom_start=10)
+    folium_map = folium.Map(location=[latitud_catalunya, longitud_catalunya], zoom_start=8)
 
     for parquing in parquings:
-        id = st.session_state.id_parking
+        id = parquing["id"]
+        st.session_state.pk_clicked = "None"
         url = f'http://127.0.0.1:8000/api/parking/{id}/plazas'
         response = req.get(url)
 
@@ -47,15 +44,35 @@ if parquings.status_code == 200:
             for ocup in ocuppied:
                 if not ocup:
                     plazas += 1
-            if (plazas > 0): colorPoint = "#008000"
+            if plazas > 0: colorPoint = "green"
+            else : colorPoint = "red"
 
             folium.Marker(
                 location=[parquing['latitude'], parquing['longitude']],
-                popup=f"{parquing['nom']}<br><br>Places disponibles: {plazas}<br><br>{'Accesible' if parquing['accesible'] else 'No accesible'}"
-                icon=folium.Icon(color=colorPoint)
+                popup=f"{parquing['nom']}<br><br>Places disponibles: {plazas}<br><br>{'Accesible' if parquing['accesible'] else 'No accesible'}",
+                icon=folium.Icon(color=colorPoint) 
             ).add_to(folium_map)
-
         else: st.error("Error al carregar les places del pàrquing")
-    folium_static(folium_map)
+    
+    st_data = st_folium(folium_map, width=800, height=600)
+
+    if st_data['last_object_clicked_popup']:
+        st.session_state.pk_clicked = st_data['last_object_clicked_popup']
+
 else: st.error("Error al carregar els pàrquings")
 
+ 
+parquing_name = "None"
+if st.session_state.pk_clicked != "None":
+    # Extraer solo el nombre del párquing
+    clicked_text = st.session_state.pk_clicked.splitlines()  # Divide en líneas
+    parquing_name = clicked_text[0]  # La primera línea es el nombre
+    st.write(f"{st.session_state.pk_clicked}")
+else:
+    st.write("Selecciona un pàrquing per veure les seves places disponibles")
+
+#actualitza id
+for parquing in parquings:
+    if parquing["nom"] == parquing_name:  # Compara el nombre del parquing
+        st.session_state.id_parking = parquing["id"]   # Asigna el ID al estado de sesión
+        break  # Detén el bucle una vez encontrado el ID
